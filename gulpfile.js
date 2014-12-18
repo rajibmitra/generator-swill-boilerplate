96354216b50df456b3bbb2d7cf14fc9483d4cfbc
Tiago Porto <me@tiagoporto.com>
2014-12-18 11:04:22 -0200

    Corrected scripts genareted name

--- gulpfile.js
+++ gulpfile.js
@@ -198,7 +198,7 @@ gulp.task('unify-scripts',  function() {
 			paths.scripts.dest + 'angular.js',
 			paths.scripts.dest + 'main-scripts.js'
 		])
-		.pipe(concat('styles.js'))
+		.pipe(concat('scripts.js'))
 		.pipe(gulp.dest(paths.scripts.dest));
 
 	var minify = gulp.src([
@@ -206,7 +206,7 @@ gulp.task('unify-scripts',  function() {
 			paths.scripts.dest + 'angular.min.js',
 			paths.scripts.dest + 'main-scripts.min.js'
 		])
-		.pipe(concat('styles.min.js'))
+		.pipe(concat('scripts.min.js'))
 		.pipe(gulp.dest(paths.scripts.dest));
 
 	return merge(unminify, minify);
